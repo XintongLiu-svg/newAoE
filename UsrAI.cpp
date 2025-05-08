3978b39c44f3fa7d04703b6a9aadd88d8e5d2d8d
liuliuQueen <liuliuQueen@noreply.gitcode.com>
2025-05-08 14:37:39 +0800

    添加总攻和敌军

---
+++ UsrAI.cpp
@@ -0,0 +1,8 @@
+#include "UsrAI.h"
+tagGame tagUsrGame;
+ins UsrIns;
+/*##########请勿修改以上部分##########*/
+void UsrAI::processData()
+{
+
+}
---
+++ UsrAI.h
@@ -0,0 +1,37 @@
+#ifndef USRAI_H
+#define USRAI_H
+
+#include "AI.h"
+
+extern tagGame tagUsrGame;
+extern ins UsrIns;
+/*##########请勿修改以上部分##########*/
+class UsrAI:public AI
+{
+public:
+    UsrAI(){this->id=0;}
+    ~UsrAI(){}
+
+
+private:
+  /***************AI通信函数，请勿修改***************/
+    void processData() override;
+    tagInfo getInfo(){return tagUsrGame.getInfo();}
+    int AddToIns(instruction ins) override
+    {
+        UsrIns.lock.lock();
+        ins.id=UsrIns.g_id;
+        UsrIns.g_id++;
+        UsrIns.instructions.push(ins);
+        UsrIns.lock.unlock();
+        return ins.id;
+    }
+    void clearInsRet() override
+    {
+        tagUsrGame.clearInsRet();
+    }
+  /***************AI通信函数，请勿修改***************/
+};
+
+
+#endif // USRAI_H
